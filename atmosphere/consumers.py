cb9e0a01072f6b50e8d14f3574c69c47e1f57c7a
Steve Gregory <contact@steve-gregory.com>
2016-03-20 01:26:54 -0700

    Set 'access_token=' rather than 'session_key=' and take advantage of 'channel_session' so that you can hash message->user

--- atmosphere/consumers.py
+++ atmosphere/consumers.py
@@ -6,25 +6,25 @@ from channels.auth import http_session_user, channel_session_user, transfer_user
 from channels.handler import AsgiHandler
 
 from channels.sessions import enforce_ordering
-
+from iplantauth.models import Token
 
 from django.http import HttpResponse
 from django.contrib.auth.models import AnonymousUser
 
 
 def disconnect_from_instances(message):
-    user = message.user
+    user = user_from_token(message)
     if isinstance(user, AnonymousUser):
         return
     logger = logging.getLogger(__name__)
     for inst in user.current_instances:
         group_name = "instance-push-%s" % inst.provider_alias
         Group(group_name).discard(message.reply_channel)
-        logger.info('Add group: %s' % group_name)
+        logger.info('Remove group: %s' % group_name)
 
 
 def connect_to_instances(message):
-    user = message.user
+    user = user_from_token(message)
     if isinstance(user, AnonymousUser):
         return
     logger = logging.getLogger(__name__)
@@ -35,7 +35,7 @@ def connect_to_instances(message):
 
 
 def latest_instance_state(message):
-    user = message.user
+    user = user_from_token(message)
     if isinstance(user, AnonymousUser):
         return
     logger = logging.getLogger(__name__)
@@ -46,7 +46,7 @@ def latest_instance_state(message):
             "status": instance.get_last_history().status.name,
 
         })
-        logger.info('Update sent for group: %s' % group_name)
+        logger.info('Update group: %s' % group_name)
 
 
 def ws_push_instance_update(instance, new_history):
@@ -59,11 +59,18 @@ def ws_push_instance_update(instance, new_history):
         }
     })
 
+def user_from_token(message):
+    if hasattr(message,'content') and 'access_token' in message.content.get('query_string',''):
+        token = message.content['query_string'].replace('access_token=','')
+        message.channel_session['token'] = token
+    if 'token' not in message.channel_session:
+        return None
+    user = Token.objects.get(key=message.channel_session['token']).user
+    return user
 
 #@channel_session
 @channel_session_user
 def ws_connect(message):
-    import ipdb;ipdb.set_trace()
     logger = logging.getLogger(__name__)
     logger.info('WebSocket connect message = %s', message.__dict__)
     connect_to_instances(message)
