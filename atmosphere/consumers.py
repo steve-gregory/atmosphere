5259ff5a21a727473b0cdd683668c6cdec62264e
Steve Gregory <contact@steve-gregory.com>
2016-03-20 22:55:41 -0700

    Reorganize, document, include 'chat' as a baseline

--- atmosphere/consumers.py
+++ atmosphere/consumers.py
@@ -12,85 +12,154 @@ from django.http import HttpResponse
 from django.contrib.auth.models import AnonymousUser
 
 
-def disconnect_from_instances(message):
+@channel_session_user
+def ws_connect(message):
+    """
+    Router for all websocket.connect
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('WebSocket connect message = %s', message.__dict__)
+    path = message.content['path']
+    if 'instance' in path:
+        return push_connect(message)
+    return chat_connect(message)
+
+
+@channel_session_user
+def ws_disconnect(message):
+    """
+    Router for all websocket.disconnect
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('WebSocket disconnect message = %s', message.__dict__)
+    path = message.channel_session['path']
+    if 'instance' in path:
+        return push_disconnect(message)
+    return chat_disconnect(message)
+
+
+@channel_session_user
+def ws_receive(message):
+    """
+    Router for all websocket.receive
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('WebSocket receive message = %s', message.__dict__)
+    path = message.channel_session['path']
+    if 'instance' in path:  # /connect/instances
+        return push_instance_updates(message)
+    return chat_reply(message)
+
+
+def user_from_token(message):
+    """
+    Same idea as 'session_key=12345'
+    this allows troposphere to 'authenticate' its WebSockets
+    """
+    if hasattr(message,'content') and 'access_token' in message.content.get('query_string',''):
+        token = message.content['query_string'].replace('access_token=','')
+        message.channel_session['token'] = token
+    if 'path' not in message.channel_session and 'path' in message.content:
+        message.channel_session['path'] = message.content['path']
+    if 'token' not in message.channel_session:
+        return None
+    user = Token.objects.get(key=message.channel_session['token']).user
+    if 'username' not in message.channel_session:
+        message.channel_session['username'] = user.username
+    return user
+
+
+def ws_push_instance(message):
+    """
+    Connected to instance-push
+    in routing.py
+    Responsible for messaging a users group
+    """
+    group_name = "push-%s" % message.content['username']
+    payload = message.content['payload']
+    Group(group_name).send({
+        "content" : payload,
+    })
+    logger = logging.getLogger(__name__)
+    logger.info('WebSocket sends message to %s = %s' % (group_name, payload))
+
+def push_disconnect(message):
+    """
+    Remove a group for this user
+    """
+    user = user_from_token(message)
+    if isinstance(user, AnonymousUser):
+        return
+    group_name = "push-%s" % message.channel_session['username']
+    Group(group_name).discard(message.reply_channel)
+    logger = logging.getLogger(__name__)
+    logger.info('Remove group: %s' % group_name)
+
+
+def push_connect(message):
+    """
+    Create a group for this user
+    """
+    user = user_from_token(message)
+    if isinstance(user, AnonymousUser):
+        return
+    group_name = "push-%s" % message.channel_session['username']
+    Group(group_name).add(message.reply_channel)
+    logger = logging.getLogger(__name__)
+    logger.info('Add group: %s' % group_name)
+
+
+def push_instance_updates(message):
+    """
+    User has sent a request to listen for updates
+    on one or more instances.
+    If instance is found, publish an 'update' with
+    last known value.
+    """
     user = user_from_token(message)
     if isinstance(user, AnonymousUser):
         return
     logger = logging.getLogger(__name__)
-    for inst in user.current_instances:
-        group_name = "instance-push-%s" % inst.provider_alias
-        Group(group_name).discard(message.reply_channel)
-        logger.info('Remove group: %s' % group_name)
-
-
-def connect_to_instances(message):
-    user = user_from_token(message)
-    if isinstance(user, AnonymousUser):
-        return
-    logger = logging.getLogger(__name__)
-    for inst in user.current_instances:
-        group_name = "instance-push-%s" % inst.provider_alias
-        Group(group_name).add(message.reply_channel)
-        logger.info('Add group: %s' % group_name)
-
-
-def latest_instance_state(message):
-    user = user_from_token(message)
-    if isinstance(user, AnonymousUser):
-        return
-    logger = logging.getLogger(__name__)
+    username = message.channel_session['username']
+    instance_match = message.content.get('text','').split(',')
     for instance in user.current_instances:
-        group_name = "instance-push-%s" % instance.provider_alias
-        Group(group_name).send({
-            "instance_id": instance.provider_alias,
-            "status": instance.get_last_history().status.name,
+        if instance.provider_alias in instance_match:
+            #FIXME: Actual implementation would change here after testing
+            logger.info("Push instance update to:%s - %s" % (username,instance.provider_alias))
+            ws_push_instance_update(
+                username,
+                instance,
+                instance.get_last_history()
+            )
 
-        })
-        logger.info('Update group: %s' % group_name)
 
-
-def ws_push_instance_update(instance, new_history):
-    user = instance.created_by
-    Channel("instance-push").send({
-        "user": user.username,
+def ws_push_instance_update(username, instance, new_history):
+    """
+    Send message to channel 'instance-push'
+    to notify listeners of the latest changes
+    """
+    channel_name = "instance-push"  # Registered in routing.py
+    Channel(channel_name).send({
+        "username": username,
         "payload": {
             "instance_id": instance.provider_alias,
             "status": new_history.status.name,
         }
     })
 
-def user_from_token(message):
-    if hasattr(message,'content') and 'access_token' in message.content.get('query_string',''):
-        token = message.content['query_string'].replace('access_token=','')
-        message.channel_session['token'] = token
-    if 'token' not in message.channel_session:
-        return None
-    user = Token.objects.get(key=message.channel_session['token']).user
-    return user
-
-#@channel_session
-@channel_session_user
-def ws_connect(message):
-    logger = logging.getLogger(__name__)
-    logger.info('WebSocket connect message = %s', message.__dict__)
-    connect_to_instances(message)
-    pass
+# Serves as a 'benchmark'
+def chat_connect(message):
+    # Add them to the right group
+    user = user_from_token(message)
+    Group("chat-%s" % user.username).add(message.reply_channel)
 
 
-@channel_session_user
-def ws_disconnect(message):
-    disconnect_from_instances(message)
+def chat_reply(message):
+    user = user_from_token(message)
+    # Echo back the content
+    Group("chat-%s" % user.username).send(message.content)
 
 
-@channel_session_user
-def ws_refresh_instance(message):
-    latest_instance_state(message)
-
-
-
-@channel_session
-def ws_push_instance(message):
-    Group("instance-push-%s"
-          % message.content['user']).send({
-        "content" : message.content['payload']
-    })
+def chat_disconnect(message):
+    user = user_from_token(message)
+    Group("chat-%s" % user.username).discard(message.reply_channel)
--- atmosphere/routing.py
+++ atmosphere/routing.py
@@ -4,7 +4,7 @@ from . import consumers
 channel_routing = {
     # If 'http.request' is not set -- It will process normally
     #"http.request": consumers.http_consumer,
-    "websocket.receive": consumers.ws_refresh_instance,
+    "websocket.receive": consumers.ws_receive,
     "websocket.connect": consumers.ws_connect,
     "websocket.disconnect": consumers.ws_disconnect,
     "instance-push": consumers.ws_push_instance,
