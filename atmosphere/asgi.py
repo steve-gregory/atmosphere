1e93e87b91275e68dd06715332bc491d9110d511
Steve Gregory <contact@steve-gregory.com>
2016-03-26 18:56:38 -0700

    Changes made to asgi.py to resemble channels-examples

--- atmosphere/asgi.py
+++ atmosphere/asgi.py
@@ -1,6 +1,13 @@
+"""
+ASGI entrypoint file for default channel layer.
+
+get_channel_layer points to channel layer configured as 'default'
+in Atmosphere's settings files.
+Note from author: You can point ASGI applications at `atmosphere.asgi:channel_layer` as their channel layer.
+"""
+
 import os
 from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atmosphere.settings")
-
 channel_layer = get_channel_layer()
