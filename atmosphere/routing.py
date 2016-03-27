ef5f8ebb44ba4f204f8b411a8a2dac15b23933af
Steve Gregory <contact@steve-gregory.com>
2016-03-26 18:57:55 -0700

    New routing.py closely resembles urls.py

--- atmosphere/routing.py
+++ atmosphere/routing.py
@@ -1,11 +1,17 @@
-from . import consumers
+from channels.routing import include
 
 
-channel_routing = {
-    # If 'http.request' is not set -- It will process normally
-    #"http.request": consumers.http_consumer,
-    "websocket.receive": consumers.ws_receive,
-    "websocket.connect": consumers.ws_connect,
-    "websocket.disconnect": consumers.ws_disconnect,
-    "instance-push": consumers.ws_push_instance,
-}
+# channel_routing is the equivalent of 'urlpatterns'
+# It allows you to distribute how channels are handled by consumers.
+# including optional matching on message attributes.
+channel_routing = [
+    # Include sub-routing from the core app.
+    include("core.routing.websocket_routing", path=r"^/ws/push/instances"),
+
+    # Custom handler for message sending
+    # Can't go in the include above as it's not got a `path` attribute to match on.
+    include("core.routing.custom_routing"),
+
+    # A default "http.request" route is always inserted by Django at the end of the routing list
+    # that routes all unmatched HTTP requests to the Django view system.
+]
