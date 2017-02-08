b5341baaf9ff6683dd3212c5e165525b05c08525
Steve Gregory <contact@steve-gregory.com>
2017-02-08 10:11:33 -0700

    Simple provider metrics (short-term, likely will be too slow to be useful.)

--- api/v2/views/site_metric.py
+++ api/v2/views/site_metric.py
@@ -1,6 +1,8 @@
 """
  RESTful Site Metrics API
 """
+import ipaddress
+
 from dateutil.parser import parse
 from django.db.models import Q
 from django.utils import timezone
@@ -13,7 +15,8 @@ from rest_framework.settings import api_settings
 from api.renderers import PandasExcelRenderer
 from api.v2.exceptions import failure_response
 from api.v2.views.base import AuthAPIViewset
-from core.models import Instance
+from core.models import Instance, Provider
+from service.driver import get_account_driver
 
 
 class SiteMetricViewSet(AuthAPIViewset):
@@ -32,13 +35,42 @@ class SiteMetricViewSet(AuthAPIViewset):
         prev_week_metrics = self._get_metrics(two_weeks_ago, last_week)
         last_month_metrics = self._get_metrics(last_month)
         prev_month_metrics = self._get_metrics(two_months_ago, last_month)
+        provider_metrics = self._get_provider_metrics()
         site_metric = {
+            "provider": provider_metrics,
             "last_week": last_week_metrics,
             "last_month": last_month_metrics,
             "previous_week": prev_week_metrics,
             "previous_month": prev_month_metrics
         }
         return Response([site_metric])
+
+    def _get_provider_metrics(self):
+        prov_metrics = {}
+        for prov in Provider.get_active():
+            if prov.type.name.lower() != 'openstack':
+                continue
+            search_public_network_args = {
+                "router:external": True,
+                "shared": True,
+                }
+            accounts = get_account_driver(prov)
+            public_networks = accounts.network_manager.list_networks(
+                **search_public_network_args)
+            public_subnet_ids = []
+            for pub_net in public_networks:
+                public_subnet_ids.extend(pub_net['subnets'])
+            subnets = accounts.network_manager.list_subnets()
+            public_subnets = [sn for sn in subnets if sn['id'] in public_subnet_ids]
+            ip_space = []
+            for psn in public_subnets:
+                ip_space.extend(ipaddress.ip_network(psn['cidr']))
+            ip_allocated = accounts.network_manager.list_floating_ips()
+            prov_metrics[str(prov.uuid)] = {
+                "floating_ips_total": len(ip_space),
+                "floating_ips_allocated": len(ip_allocated),
+            }
+        return prov_metrics
 
     def _get_metrics(self, start_date, start_date_end=None):
         instances = Instance.objects.filter(start_date__gt=start_date)
