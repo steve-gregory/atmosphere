9b01c9d45fa6184b5ac34ddaa3cca5cdd668c666
Connor Osborn <cdosborn@email.arizona.edu>
2017-06-05 18:05:06 +0000

    Allow admins to patch a users quota

--- api/v2/views/identity.py
+++ api/v2/views/identity.py
@@ -2,11 +2,12 @@ from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from rest_framework import status
 
-from core.models import Identity, Group
+from core.models import Identity, Group, Quota
 
 from api.v2.serializers.details import IdentitySerializer
 from api.v2.views.base import AuthModelViewSet
 from api.v2.views.mixins import MultipleFieldLookup
+from service.tasks import admin as admin_task
 
 
 class IdentityViewSet(MultipleFieldLookup, AuthModelViewSet):
@@ -17,7 +18,7 @@ class IdentityViewSet(MultipleFieldLookup, AuthModelViewSet):
     lookup_fields = ("id", "uuid")
     serializer_class = IdentitySerializer
     queryset = Identity.objects.all()
-    http_method_names = ['get', 'head', 'options', 'trace']
+    http_method_names = ['get', 'head', 'options', 'trace', 'patch']
 
     @detail_route(methods=['GET'])
     def export(self, request, pk=None):
@@ -54,3 +55,28 @@ class IdentityViewSet(MultipleFieldLookup, AuthModelViewSet):
                 target_username = self.request.GET.get('username')
                 return self.queryset_by_username(target_username)
         return self.queryset_by_username(user.username)
+
+    def update(self, request, pk=None, partial=False):
+        if not request.user.is_admin():
+            return Response("Insufficient privileges", status=status.HTTP_403_FORBIDDEN)
+
+        if not pk:
+            return Response("Missing identity primary key",
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        identity = Identity.objects.get(uuid=pk)
+        serializer = self.get_serializer_class()
+
+        data = request.data
+        if 'quota' in data and 'id' in data.get('quota'):
+            quota_id = data.get('quota').get('id')
+            identity.quota = Quota.objects.get(id=quota_id)
+            identity.save()
+            admin_task.set_provider_quota.delay(str(identity.uuid))
+
+        serialized = serializer(identity,
+                                context={'request': self.request})
+
+        return Response(
+                serialized.data,
+                status=status.HTTP_200_OK)
